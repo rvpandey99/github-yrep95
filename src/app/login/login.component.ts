c63af0e607f312956fdf5e3de7461dc066065e86
rvpandey99 <rvpandey99@gmail.com>
2019-09-05 20:39:34 +0530

    3 files modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  user:FormGroup;
 
+  isValid(controlName){
+    return this.user.get(controlName).invalid && this.user.get(controlName).touched;
+  }
   constructor() { }
 
   ngOnInit() {
+    this.user = new FormGroup({
+    userId: new FormControl('', [ Validators.required, Validators.minLength(3) ]),
+    password: new FormControl('', [ Validators.required, Validators.minLength(6)]),
+    });
   }
 
 }
